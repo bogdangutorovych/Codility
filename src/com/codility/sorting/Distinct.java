1278b2cb441a7efd34676f07ca6325edb8cee575
Bogdan Gutorovych <bogdan.gutorovych@gmail.com>
2017-10-14 11:30:29 +0300

    first sorting task

---
+++ src/com/codility/sorting/Distinct.java
@@ -0,0 +1,25 @@
+package com.codility.sorting;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Distinct {
+    
+    public int solution(int[] A) {
+        Set<Integer> set= new HashSet<Integer>();
+        for (int i = 0; i < A.length; i++) {
+            if (!set.contains(A[i])) {
+                set.add(A[i]);
+            } 
+            
+        }
+        return set.size();
+    }
+    
+    public int solution2(int[] A) {
+        return Arrays.stream(A).distinct().toArray().length;
+    }
+    
+}
